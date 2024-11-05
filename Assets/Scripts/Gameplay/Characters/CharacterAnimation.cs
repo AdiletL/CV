05841154fca5b45db81cf20cf49db840d8ced3a6
AdiletL <satybaldievadilet8@gmail.com>
2024-11-05 17:52:16 +0600

    optimized character animation

--- Assets/Scripts/Gameplay/Characters/CharacterAnimation.cs
+++ Assets/Scripts/Gameplay/Characters/CharacterAnimation.cs
@@ -8,40 +8,37 @@ namespace Character
         [SerializeField] protected Animator animator;
         
         protected AnimationClip currentClip;
-        protected AnimatorClipInfo[] currentClips;
-        
         protected float currentSpeed;
 
-        protected const string TRANSITION_SPEED = "speed";
+        private Dictionary<AnimationClip, float> clipLengths = new(); // Кэш длины клипов
+        private const string TRANSITION_SPEED = "speed";
 
         public virtual void Initialize()
         {
-
+            
         }
 
         public void ChangeAnimation(AnimationClip clip, float division = 1, float duration = 1,
             float transitionDuration = .1f, bool force = false)
         {
-            currentClips = animator.GetCurrentAnimatorClipInfo(0);
+            if (clip == null || (!force && currentClip == clip)) return;
 
-            if (!clip)
-            {
-                return;
-            } 
-            else if (!force && currentClips.Length != 0)
-            {
-                currentClip = animator.GetCurrentAnimatorClipInfo(0)[0].clip;
-                if (clip == currentClip) return;
-            }
-
-            SetSpeedAnimation(ref clip, division, duration);
-            animator?.CrossFadeInFixedTime(clip?.name, transitionDuration, 0);
+            SetSpeedAnimation(clip, division, duration);
+            
+            animator.CrossFadeInFixedTime(clip.name, transitionDuration, 0);
+            currentClip = clip;
         }
 
-        protected void SetSpeedAnimation(ref AnimationClip clip, float division = 1, float duration = 1)
+        protected void SetSpeedAnimation(AnimationClip clip, float division = 1, float duration = 1)
         {
-            float speed = (clip.length / division) * duration;
-            if (currentSpeed == speed) return;
+            if (!clipLengths.TryGetValue(clip, out float clipLength))
+            {
+                clipLength = clip.length;
+                clipLengths[clip] = clipLength; // Кэшируем длину клипа
+            }
+
+            float speed = (clipLength / division) * duration;
+            if (Mathf.Approximately(currentSpeed, speed)) return;
 
             animator.SetFloat(TRANSITION_SPEED, speed);
             currentSpeed = speed;
